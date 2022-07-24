143c01a707ea0f3568b9241021cc84da4b48b901
suremarc <8771538+suremarc@users.noreply.github.com>
2022-07-24 02:08:26 -0400

    broken impl of value iteration + refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(generic_const_exprs)]
+#![feature(slice_as_chunks)]
+
 use num_bigint::BigUint;
 use ordered_float::OrderedFloat;
 use rand::{distributions::Bernoulli, prelude::ThreadRng, Rng};
@@ -74,6 +77,36 @@ pub fn weak_compositions<const K: usize>(n: u8) -> Vec<[u8; K]> {
     result
 }
 
+pub fn betas<const K: usize>(n: usize) -> Vec<[Beta; K]>
+where
+    [(); K * 2]:,
+{
+    weak_compositions::<{ K * 2 }>(n.try_into().unwrap())
+        .into_iter()
+        .map(|cell| {
+            let mut betas: [Beta; K] = [Default::default(); K];
+            for (i, &[successes, failures]) in unsafe { cell.as_chunks_unchecked::<2>() }
+                .iter()
+                .enumerate()
+            {
+                betas[i] = Beta {
+                    successes,
+                    failures,
+                }
+            }
+
+            betas
+        })
+        .collect()
+}
+
+pub fn betas_recursive<const K: usize>(n: usize) -> Vec<[Beta; K]>
+where
+    [(); K * 2]:,
+{
+    (0..=n).rev().flat_map(betas::<K>).collect()
+}
+
 #[derive(Debug, Clone)]
 pub struct State<const K: usize> {
     pub p: [f64; K],
@@ -129,9 +162,69 @@ impl<'a, const K: usize> Belief<'a, K> {
         outcome
     }
 
+    pub fn expected_reward<F>(&self, action: usize, mut lookahead: F) -> f64
+    where
+        F: FnMut(&[Beta; K]) -> f64,
+    {
+        let p_success = self.dist[action].mean(self.alpha, self.beta);
+        let mut reward = p_success;
+        for (outcome, prob) in [(true, p_success), (false, 1. - p_success)] {
+            let mut dist = self.dist;
+            // println!("{dist:#?}");
+            dist[action] = dist[action].posterior(outcome);
+            reward += prob * lookahead(&dist);
+        }
+
+        reward
+    }
+
+    pub fn value_iterate(&self, n: usize, tolerance: f64) -> BTreeMap<[Beta; K], f64>
+    where
+        [(); K * 2]:,
+    {
+        let states = betas_recursive::<K>(n);
+        println!("{states:#?}");
+        let mut value: BTreeMap<&[Beta; K], f64> = states.iter().map(|beta| (beta, 0.)).collect();
+        // println!("{value:#?}");
+
+        let mut delta: f64 = 0.;
+        loop {
+            for state in states.iter() {
+                let new = (0..K)
+                    .map(|action| self.expected_reward(action, |dist| *value.get(dist).unwrap()))
+                    .max_by_key(|&reward| OrderedFloat::from(reward))
+                    .unwrap();
+                let orig = value.insert(state, new).unwrap();
+
+                delta = delta.max((new - orig).abs());
+            }
+
+            if delta < tolerance {
+                return value
+                    .into_iter()
+                    .map(|(&beta, value)| (beta, value))
+                    .collect();
+            }
+            println!("{delta}");
+            println!("{value:#?}");
+        }
+    }
+
+    pub fn best_action(&self, value: &BTreeMap<[Beta; K], f64>) -> usize {
+        (0..K)
+            .map(|action| {
+                (
+                    action,
+                    self.expected_reward(action, |dist| *value.get(dist).unwrap()),
+                )
+            })
+            .max_by_key(|&(_, reward)| OrderedFloat::from(reward))
+            .unwrap()
+            .0
+    }
+
     fn bellman_recurse(
-        alpha: f64,
-        beta: f64,
+        &self,
         dist: &[Beta; K],
         n: usize,
         memoized: &mut BTreeMap<[Beta; K], (usize, f64)>,
@@ -146,16 +239,12 @@ impl<'a, const K: usize> Belief<'a, K> {
                 // println!("1");
                 let (best_action, reward) = (0..K)
                     .map(|action| {
-                        let p_success = dist[action].mean(alpha, beta);
-                        let mut reward = p_success;
-                        for (outcome, prob) in [(true, p_success), (false, 1. - p_success)] {
-                            let mut dist = *dist;
-                            dist[action] = dist[action].posterior(outcome);
-                            reward +=
-                                prob * Self::bellman_recurse(alpha, beta, &dist, n - 1, memoized);
-                        }
-
-                        (action, reward)
+                        (
+                            action,
+                            self.expected_reward(action, |dist| {
+                                Self::bellman_recurse(self, dist, n - 1, memoized)
+                            }),
+                        )
                     })
                     .max_by_key(|&(_, reward)| OrderedFloat::from(reward))
                     .unwrap();
@@ -176,7 +265,7 @@ impl<'a, const K: usize> Belief<'a, K> {
         n: usize,
         memoized: &mut BTreeMap<[Beta; K], (usize, f64)>,
     ) -> Option<(usize, f64)> {
-        Self::bellman_recurse(self.alpha, self.beta, &self.dist, n, memoized);
+        self.bellman_recurse(&self.dist, n, memoized);
         memoized.get(&self.dist).copied()
     }
 }
@@ -188,28 +277,30 @@ fn main() {
 
     let mut average_score: f64 = 0.;
     const I: usize = 1;
-    const N: usize = 100;
+    const N: usize = 3;
     for i in 0..I {
         let mut belief = Belief::<2>::new(&state, r.clone(), 1., 1.);
 
-        let mut memoized = BTreeMap::new();
+        // let mut memoized = BTreeMap::new();
+        let value = belief.value_iterate(N, 1.);
         let t0 = std::time::Instant::now();
 
         let mut score = 0;
         for n in 0..N {
-            let (a, expected_reward) = belief.best_with_memoized(N - n, &mut memoized).unwrap();
-            let expected_score = score as f64 + expected_reward;
+            // let (a, expected_reward) = belief.best_with_memoized(N - n, &mut memoized).unwrap();
+            let a = belief.best_action(&value);
+            // let expected_score = score as f64 + expected_reward;
             let outcome = belief.take(a);
             score += outcome as u64;
-            if i == 0 {
-                eprintln!("{n}: ({a}, {expected_score:.2}) -> {outcome} ({score})");
-            }
+            // if i == 0 {
+            //     eprintln!("{n}: ({a}, {expected_score:.2}) -> {outcome} ({score})");
+            // }
         }
 
         if i == 0 {
             eprintln!(
                 "# of states: {} ({}ms)",
-                memoized.len(),
+                value.len(),
                 t0.elapsed().as_millis()
             );
         }
